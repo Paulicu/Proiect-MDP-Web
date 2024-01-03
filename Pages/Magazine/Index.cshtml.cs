e877611e86b06f895a1d652b790ea05ce084722c
Groza Tudor <tudorgroza1969@yahoo.com>
2024-01-03 16:19:07 +0200

    v4.1 - am adaugat sortare la nume si firma pentru rachete si am adaugat buton de select pentru magazin

--- Pages/Magazine/Index.cshtml.cs
+++ Pages/Magazine/Index.cshtml.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Proiect_MDP_Web.Data;
 using Proiect_MDP_Web.Models;
+using Proiect_MDP_Web.ViewModels;
 
 namespace Proiect_MDP_Web.Pages.Magazine
 {
@@ -21,11 +23,26 @@ namespace Proiect_MDP_Web.Pages.Magazine
 
         public IList<Magazin> Magazin { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public MagazinIndexData MagazinData { get; set; }
+        public int MagazinID { get; set; }
+        public int RachetaID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? RachetaID)
         {
-            if (_context.Magazin != null)
+            MagazinData = new MagazinIndexData();
+            MagazinData.Magazine = await _context.Magazin
+            .Include(i => i.Rachete)
+            .ThenInclude(c => c.Firma)
+            .OrderBy(i => i.DenumireMagazin)
+            .ToListAsync();
+
+            if (id != null)
             {
-                Magazin = await _context.Magazin.ToListAsync();
+                //Magazin = await _context.Magazin.ToListAsync();
+                MagazinID = id.Value;
+                Magazin magazin = MagazinData.Magazine
+                .Where(i => i.ID == id.Value).Single();
+                MagazinData.Rachete = magazin.Rachete;
             }
         }
     }
