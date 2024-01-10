8946b1e7889fc60b0cfa00a9f0904d8f3ede39a5
Paul Adrian <uifalean.paul96@gmail.com>
2024-01-10 21:37:31 +0200

    v6.6 - Add Text Area for Recenzii.

--- Models/Categorie.cs
+++ Models/Categorie.cs
@@ -1,8 +1,12 @@
-﻿namespace Proiect_MDP_Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Proiect_MDP_Web.Models
 {
     public class Categorie
     {
         public int ID { get; set; }
+
+        [Display(Name = "Denumire Categorie")]
         public string DenumireCategorie { get; set; }
         public ICollection<CategorieRacheta>? CategoriiRacheta { get; set; }
     }
